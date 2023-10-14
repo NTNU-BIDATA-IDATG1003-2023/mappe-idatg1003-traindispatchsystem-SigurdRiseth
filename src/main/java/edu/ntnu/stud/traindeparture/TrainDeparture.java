43f0b07ca1c3f91ac1a7c8e59aacca4ad287b82c
Sigurd Riseth <sigurdriseth@Sigurds-MacBook-Air.local>
2023-10-14 11:33:18 +0200

    Added fields to TrainDeparture

--- src/main/java/edu/ntnu/stud/traindeparture/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/traindeparture/TrainDeparture.java
@@ -1,4 +1,21 @@
 package edu.ntnu.stud.traindeparture;
 
+/**
+ * Class for the train departures.
+ *
+ * Will store information such as train number, line, departure time, delay, destination and what track it will depart from.
+ *
+ * @author sigurdriseth
+ * @version 0.0.1
+ * @since 14.10.2023
+ */
 public class TrainDeparture {
+
+    private int track;
+    private int trainNumber;
+    private String line;
+    private Clock departureTime; // use a Clock class to store departureTime and delay.
+    private Clock delay;
+    private String destination;
+
 }
