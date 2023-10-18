2f4ad2eba8e0c0dadcf08b9ee3f5c2563108e57d
Sigurd Riseth <sigurd.riseth@gmail.com>
2023-10-18 15:28:42 +0200

    Added the class Station which keeps track of a hashmap of the TrainDeparture instances and the clock. The plan is for the hashmap to contain only the train which have yet to depart.

---
+++ src/main/java/edu/ntnu/stud/station/Station.java
@@ -0,0 +1,46 @@
+package edu.ntnu.stud.station;
+import edu.ntnu.stud.traindeparture.TrainDeparture;
+import java.time.LocalTime;
+import java.util.HashMap;
+
+/**
+ * Class for the train station.
+ * <p>In charge of keeping track of the time and a list of all TrainDepartures yet to depart.</p>
+ */
+public class Station {
+
+  private LocalTime time;
+  private HashMap<Integer, TrainDeparture> trainDepartures;
+
+  /**
+   * Constructor that sets the time to midnight and creates a new HashMap for the train departures.
+   */
+  public Station() {
+    this.time = LocalTime.of(0, 0);
+    this.trainDepartures = new HashMap<>();
+  }
+
+  /**
+   * Sets the time.
+   * @param time
+   */
+  public void setClock(LocalTime time) {
+    this.time = time;
+  }
+
+  /**
+   * Returns the time.
+   * @return time
+   */
+  public LocalTime getClock() {
+    return this.time;
+  }
+
+  /**
+   * Adds a TrainDeparture to the trainDepartures HashMap.
+   * @param trainDeparture
+   */
+  public void addTrainDeparture(TrainDeparture trainDeparture) {
+    this.trainDepartures.put(trainDeparture.getTrainNumber(), trainDeparture);
+  }
+}
