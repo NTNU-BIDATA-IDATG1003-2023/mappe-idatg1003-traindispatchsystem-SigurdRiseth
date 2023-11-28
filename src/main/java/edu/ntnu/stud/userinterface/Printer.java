5267c2b73d737b22940afe279c2083503858f8a3
Sigurd Riseth <sigurd.riseth@gmail.com>
2023-11-28 12:49:21 +0100

    Finished the java documentation for the Printer class.

--- src/main/java/edu/ntnu/stud/userinterface/Printer.java
+++ src/main/java/edu/ntnu/stud/userinterface/Printer.java
@@ -12,11 +12,12 @@ import java.util.Iterator;
  */
 public class Printer {
 
+
+  private final String TABLEFORMAT = "%-15s %-10s %-20s %-20s %-15s %-15s %n";
+
   /**
    * Prints all the options the user can choose from.
    */
-  private final String TABLEFORMAT = "%-15s %-10s %-20s %-20s %-15s %-15s %n";
-
   public void printOptions() {
     System.out.println(String.join("\n",
         "-------------------------------------------",
@@ -37,6 +38,7 @@ public class Printer {
 
   /**
    * Prints a String of all departures yet to depart.
+   * <p>Uses a iterator of train departures as parameter.</p>
    */
   public void printAllDepartures(Iterator<TrainDeparture> iterator) {
     StringBuilder result = new StringBuilder();
@@ -50,6 +52,13 @@ public class Printer {
     System.out.println(result);
   }
 
+  /**
+   * Formats a train departure to the table format.
+   * <p>The track will not be shown if it equals -1 and the delay will not be shown if it is 00:00.</p>
+   *
+   * @param trainDeparture The train departure to be formatted
+   * @return The formatted train departure
+   */
   private String formatTrainToTable(TrainDeparture trainDeparture) {
     return String.format(TABLEFORMAT,
         trainDeparture.getTrainNumber(),
@@ -61,8 +70,7 @@ public class Printer {
   }
 
   /**
-   * Prints all departures to a given destination. The destination can be given in any
-   * capitalisation. If no departures are found, a message is printed.
+   * Prints all departures to a given destination. If no departures are found, a message is printed.
    *
    * @param destination The destination to search for
    */
@@ -153,6 +161,7 @@ public class Printer {
 
   /**
    * Prints information about a train departure.
+   * <p>The track will not be showed if it equals -1, and the delay will not be shown if it is 00:00.</p>
    *
    * @param train The train departure to be printed
    */
@@ -168,65 +177,120 @@ public class Printer {
     System.out.println((train.getDelay() == LocalTime.of(0, 0)) ? "No delay" : train.getDelay());
   }
 
+  /**
+   * Prints a message saying the menu choice is out of bounds.
+   */
   public void printInvalidChoice() {
     System.err.println("Please enter a valid number. The number should be between 0 and 10");
   }
 
+  /**
+   * Prints a message saying that no train departure has been created yet and only choice 4, 10 and 0 is allowed.
+   */
   public void printNoTrains() {
     System.err.println("No TrainDepartures created yet. Only option 4, 10, or 0 is allowed.");
   }
 
+  /**
+   * Prints a goodbye message saying the application has been terminated.
+   */
   public void printCloseApp() {
     System.out.println("Thank you for using the train dispatch app!");
     System.out.println("The application has now been terminated.");
   }
 
+  /**
+   * Prints the clock.
+   *
+   * @param clock The clock at the station
+   */
   public void printClock(LocalTime clock) {
     System.out.println("The clock is now " + clock);
   }
 
+  /**
+   * Prints a message that the train has been removed.
+   */
   public void printTrainRemoved() {
     System.out.println("Train has been removed.");
   }
 
+  /**
+   * Prints a message that the train has been removed as it was delayed over midnight.
+   */
   public void printTrainRemovedByDelay() {
     System.out.println("Train has been removed as it was delayed over midnight.");
   }
 
+  /**
+   * Prints a message that no train to the given destination was found.
+   *
+   * @param destination The destination that was searched for
+   */
   public void printNoTrainFound(String destination) {
     System.err.println("No train to " + destination + " was found.");
   }
 
+  /**
+   * Prints a message that the train has been added.
+   */
   public void printTrainAdded() {
     System.out.println("Train departure has been added!");
   }
 
+  /**
+   * Prints an error message saying the input time can not be before the station clock.
+   *
+   * @param clock The clock at the station
+   */
   public void printBeforeClock(LocalTime clock) {
     System.err.println("You can not input a time before "
         + clock + ". Please try again.");
   }
 
+  /**
+   * Prints a welcome message when the application starts.
+   */
   public void printWelcomeMessage() {
     System.out.println("--------------------------------------------------");
     System.out.println("Welcome to the train dispatch app!");
   }
 
+  /**
+   * Prints a confirmation message that the delay has been changed.
+   */
   public void printDelayChanged() {
     System.out.println("Delay changed.");
   }
 
+  /**
+   * Prints a confirmation message that the track has been removed.
+   */
   public void printTrackRemoved() {
     System.out.println("Track removed.");
   }
 
+  /**
+   * Prints a confirmation message that the track has been changed.
+   *
+   * @param track The new track
+   */
   public void printTrackChanged(String track) {
     System.out.println("Track changed to " + track);
   }
 
+  /**
+   * Prints a message that the clock has been changed.
+   *
+   * @param time The new time
+   */
   public void printClockChanged(LocalTime time) {
     System.out.println("Clock changed to " + time);
   }
 
+  /**
+   * Prints a message that the clock was not changed as the user tried to set it backwards.
+   */
   public void printClockNotChanged() {
     System.err.println("Clock was not changed. Can not set clock backwards. Please try again.");
   }
